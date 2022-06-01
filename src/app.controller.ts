f6a55c21700aceebf9d958316b4cdd670d4913db
alan <alan89757@163.com>
2022-06-01 18:23:43 +0800

    控制器

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  @Get()
+  findAll(@Req() request: Request): string {
+    console.log(request);
+    return 'welcome findAll';
+  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
