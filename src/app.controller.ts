5df79f06c2312680dbabdc354e4e26e69094f57f
alan <alan89757@163.com>
2022-06-02 05:51:41 +0800

    请求路径

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/list')
+  list(): string {
+    return 'hello list';
   }
 }
