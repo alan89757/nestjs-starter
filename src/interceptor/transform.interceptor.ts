236a6c26849847be34dbd0c70a2fc9e1d7738bcb
alan <alan89757@163.com>
2022-06-02 13:05:20 +0800

    eslint

--- src/interceptor/transform.interceptor.ts
+++ src/interceptor/transform.interceptor.ts
@@ -1,17 +1,29 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 export interface Response<T> {
   data: T;
 }
 @Injectable()
-export class TransformInterceptor<T> implements NestInterceptor<T, Response<T>> {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
-    console.log("TransformInterceptor")
-    return next.handle().pipe(map(data => {
-      console.log(data);
-      data['detail'] = "库存不足";
-      return data;
-    }));
+export class TransformInterceptor<T>
+  implements NestInterceptor<T, Response<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    console.log('TransformInterceptor');
+    return next.handle().pipe(
+      map((data) => {
+        console.log(data);
+        typeof data === 'object' ? (data['detail'] = '库存不足') : '';
+        return data;
+      }),
+    );
   }
 }
