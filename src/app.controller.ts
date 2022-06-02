1a3f329f7321a9f3c6f5afde1638ab3f481a21ef
alan <alan89757@163.com>
2022-06-02 08:48:30 +0800

    异常过滤

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -13,16 +13,37 @@ import {
   HttpCode,
   Header,
   Redirect,
+  HttpException,
+  HttpStatus,
+  NotFoundException,
+  GatewayTimeoutException,
 } from '@nestjs/common';
 import { Request } from 'express';
 import { AppService } from './app.service';
 import { CatsService } from './cats/cats.service';
+import { ForbiddenException } from './exception/forbidden.exception';
+import { HttpExceptionFilter } from './filter/http-exception.filter';
 
 @Controller('app')
 export class AppController {
   constructor(private readonly catsService: CatsService) {}
-  @Get('hello')
-  getHello(): string {
-    return this.catsService.getCats();
+  @Get('forbidden')
+  async findAll() {
+    throw new HttpException('Forbidden111', HttpStatus.FORBIDDEN);
+  }
+  @Get('custom-forbidden')
+  async customForbindden() {
+    throw new ForbiddenException();
+  }
+  @Get('NotFoundException')
+  async NotFoundException() {
+    throw new NotFoundException(HttpStatus.NOT_FOUND, '找不到对应的资源');
+  }
+  @Get('GatewayTimeoutException')
+  async GatewayTimeoutException() {
+    throw new GatewayTimeoutException({
+      status: HttpStatus.GATEWAY_TIMEOUT,
+      error: '网关获取超时',
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './filter/any-exception.filter';
+import { HttpExceptionFilter } from './filter/http-exception.filter';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(LoggerMiddleware);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  // app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(3000);
 }
 bootstrap();
